2a8b6a3ca15f818dcbaa359bd47ec0a39f6c1d7a
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-09-12 21:05:31 -0400

    Update LeadershipProfile.jsx

--- src/components/LeadershipProfile.jsx
+++ src/components/LeadershipProfile.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function LeadershipProfile({headshot, name, title, linkedin, yearsActive, bio}) {
   return (
     <div>
-        <img src={headshot} alt="" />
+        {/* <img src={headshot} alt="" />
         <span>{name}</span>
         <span>{title}</span>
         <span>{yearsActive}</span>
         <p>{bio}</p>
-        <a href={linkedin}>LinkedIn</a>
+        <a href={linkedin}>LinkedIn</a> */}
     </div>
   )
 }
