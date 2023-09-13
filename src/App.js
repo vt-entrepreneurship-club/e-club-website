05b1d39a90d116ccbe569ecc845fc6aed92e235e
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-09-13 17:40:24 -0400

    uploaded slides

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Leadership from "./pages/Leadership";
 import HomePage from "./pages/HomePage";
+import Slides from "./pages/Slides";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route index element={<HomePage />} />
           <Route path='/leadership' element={<Leadership/>}></Route>
+          <Route path='/slides' element={<Slides/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
