821dd2dc241867b0086e8f2e7ce092fdf9370194
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-09-13 23:14:22 -0400

    Removed slides

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import NavBar from "./components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Leadership from "./pages/Leadership";
 import HomePage from "./pages/HomePage";
-import Slides from "./pages/Slides";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Routes>
           <Route index element={<HomePage />} />
           <Route path='/leadership' element={<Leadership/>}></Route>
-          <Route path='/slides' element={<Slides/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
