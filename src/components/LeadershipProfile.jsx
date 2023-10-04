a75f1f86c649e0b78df1db8894c3ab7eed44fb7a
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-10-04 18:36:04 -0400

    Finished leadership page

--- src/components/LeadershipProfile.jsx
+++ src/components/LeadershipProfile.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
 
 function LeadershipProfile({headshot, name, title, linkedin, yearsActive, bio}) {
   return (
-    <div>
-        <img className='w-1/6 lg:float-left lg:mr-12' src={headshot} alt="" />
-        <div className="float-left">
+    <div className='mb-20'>
+        <img className='lg:w-1/4 lg:float-left' src={headshot} alt="" />
+        <div className="lg:float-left lg:ml-6 mt-6">
           <span className='lg:float-left text-2xl text-primary'>{name}</span>
           <br />
           <div className="clear-left"></div>
-          <span className=''>{title}</span>
+          <span className='text-xl'>{title}</span>
           <br />
-          {/* <span>{yearsActive}</span>
-          <p>{bio}</p> */}
-          <a href={linkedin}>LinkedIn</a>
+            <a className='btn mt-6' href={linkedin} target='_blank'>
+              <BsLinkedin className='text-primary text-2xl'></BsLinkedin>
+              Connect
+            </a>
+          
         </div>
-        
+        <div className="clear-left"></div>
     </div>
   )
 }
--- src/pages/Leadership.jsx
+++ src/pages/Leadership.jsx
@@ -1,22 +1,59 @@
 import React from 'react'
 import ebenezer_zergabachew from '../assets/leadership_photos/ebenezer_zergabachew.jpg'
+import sam_radocha from '../assets/leadership_photos/sam_radocha.jpg'
+import brody_emery from '../assets/leadership_photos/brody_emery.jpg'
+import henry_sours from '../assets/leadership_photos/henry_sours.jpg'
+import jeremiah_hohn from '../assets/leadership_photos/jeremiah_hohn.jpg'
+import sid from '../assets/leadership_photos/sid.jpg'
+import mason_butler from '../assets/leadership_photos/mason_butler.png'
 import LeadershipProfile from '../components/LeadershipProfile'
 
 function Leadership() {
 
   const leadershipData = [
     {
+      headshot: sam_radocha,
+      name: "Samuel Radocha",
+      title: "President",
+      linkedin: "https://www.linkedin.com/in/samuel-radocha/"
+    },{
       headshot: ebenezer_zergabachew,
       name: "Ebenezer Zergabachew",
       title: "Head of Technology",
-      linkedin: "https://www.linkedin.com/in/ebenezer-zergabachew-aa41011a9/"
-    }
+      linkedin: "https://www.linkedin.com/in/ebenezer-zergabachew"
+    },{
+      headshot: henry_sours,
+      name: "Henry Sours",
+      title: "Marketing Director",
+      linkedin: "https://www.linkedin.com/in/henrysours87/"
+    },{
+      headshot: jeremiah_hohn,
+      name: "Jeremiah Hohn",
+      title: "Junior Marketing Director",
+      linkedin: "https://www.linkedin.com/in/jeremiahhohn/"
+    },{
+      headshot: brody_emery,
+      name: "Brody Emery",
+      title: "Project Manager",
+      linkedin: "https://www.linkedin.com/in/brody-emery-88539321b/"
+    },{
+      headshot: sid,
+      name: "Siddarth Ashok",
+      title: "Project Manager",
+      linkedin: "https://www.linkedin.com/in/siddarth-ashok/"
+    },{
+      headshot: mason_butler,
+      name: "Mason Butler",
+      title: "Videographer / Photographer",
+      linkedin: "https://www.linkedin.com/in/mason-butler/"
+    },
   ]
 
   return (
     <div className='container'>
-       <h1 className='text-4xl mb-20'>Entrepreneurship Club's Leadership</h1> 
-        {
+       <h1 className='text-4xl mb-20'>Leadership</h1> 
+       <div className="lg:grid lg:grid-cols-2">
+       {
           leadershipData.map((leader) => (
             <LeadershipProfile
             name={leader.name}
@@ -26,6 +63,8 @@ function Leadership() {
             ></LeadershipProfile>
           ))
         }
+       </div>
+        
     </div>
   )
 }
